8dc6fcb2e00008b063832781145126461ad0697b
Chang Chen Chien <e850506@gmail.com>
2021-12-06 20:42:46 +0800

    Add implementation: no-jobChan-with-for-range-worker

--- pool.go
+++ pool.go
@@ -1,3 +1,5 @@
+// This version of naivepool use pool.Schedule to send job directly to pool.workerChan,
+// The worker use for-range pattern to receive job from Pool until channel is closed.
 package naivepool
 
 import (
@@ -9,17 +11,21 @@ import (
 type jobFunc func()
 
 type Pool struct {
-	jobChan   chan jobFunc   // We use jobChan to communicate between caller of Pool and Pool.
-	tokenChan chan struct{}  //  token controls the maximum amount of workers inside Pool.
-	wg        sync.WaitGroup // Use waitgroup to wait for workers done its job and retire.
+	jobChan    chan jobFunc // We use jobChan to communicate between caller of Pool and Pool.
+	workerChan chan jobFunc // workers conatains channel to communicate with each worker.
+	maxWorkers int
+	wg         sync.WaitGroup // Use waitgroup to wait for workers done its job and retire.
 }
 
+// Make our workerChan a buffered channel.
+const workerChanSize int = 20
+
 // New inits goroutine pool with capacity of jobchan and workerchan.
-// bufSize means the maximum number of jobs inside the buffer.
 func New(bufSize, maxWorkers int) *Pool {
 	p := &Pool{
-		jobChan:   make(chan jobFunc, bufSize),
-		tokenChan: make(chan struct{}, maxWorkers),
+		jobChan:    make(chan jobFunc, bufSize),
+		workerChan: make(chan jobFunc, workerChanSize),
+		maxWorkers: maxWorkers,
 	}
 
 	return p
@@ -27,24 +33,20 @@ func New(bufSize, maxWorkers int) *Pool {
 
 // Start starts dispatching jobs to workers.
 func (p *Pool) Start(ctx context.Context) {
-	// Fill the tokenChan with maxWorkers
-	for i := 0; i < cap(p.tokenChan); i++ {
-		p.tokenChan <- struct{}{}
+	// TODO: Dynamic add or purge workers
+	for i := 0; i < p.maxWorkers; i++ {
+		p.wg.Add(1)
+		// set up channel between pool and worker
+		go p.work(ctx)
 	}
 
+	// Monitor goroutine: receive cancellation from ctx, and close workerChan
+	// to inform workers to retire.
 	go func() {
-		for {
-			select {
-			// Received a job.
-			// Dispatch it to workers.
-			case job := <-p.jobChan:
-				// block until a token is available
-				<-p.tokenChan
-				p.wg.Add(1)
-				go p.work(job)
-			case <-ctx.Done():
-				return
-			}
+		select {
+		case <-ctx.Done():
+			close(p.workerChan)
+			return
 		}
 	}()
 }
@@ -56,14 +58,13 @@ func (p *Pool) Wait() {
 
 // Schedule sends the job the p.jobChan.
 func (p *Pool) Schedule(job jobFunc) {
-	p.jobChan <- job
+	p.workerChan <- job
 }
 
-func (p *Pool) work(job jobFunc) {
-	defer func() {
-		// Add back one token
-		p.tokenChan <- struct{}{}
-		p.wg.Done()
-	}()
-	job()
+// work executes the job received from p.workerChan.
+func (p *Pool) work(ctx context.Context) {
+	defer p.wg.Done()
+	for f := range p.workerChan {
+		f()
+	}
 }
